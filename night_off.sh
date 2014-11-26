824b3efdefc3a0a8f4040209c7b1cc747cb97de8
Jesse Bishop <jessebishop@razberrypi.(none)>
2014-11-26 12:57:49 -0500

    more hackish starts

---
+++ night_off.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/jessebishop/scripts/home_automation/light_controller.py 2 0 0 
---
+++ night_on.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/local/bin/sunwait sun down 41.569604N 70.57333W; /home/jessebishop/scripts/home_automation/light_controller.py 2 0 99    
