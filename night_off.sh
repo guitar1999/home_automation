b6fe7943a897ccef4ca573a6d22c404eba00e76f
guitar1999 <jessebishop@yahoo.com>
2019-12-02 18:15:58 -0500

    add new dimmer

--- night_off.sh
+++ night_off.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 /home/jessebishop/git/home_automation/light_controller.py 3 0 0
 /home/jessebishop/git/home_automation/light_controller.py 6 0 0
+/home/jessebishop/git/home_automation/light_controller.py 8 0 0
--- night_on.sh
+++ night_on.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/local/bin/sunwait sun down 43.921510N 70.084008W; /home/jessebishop/git/home_automation/light_controller.py 3 0 90; /home/jessebishop/git/home_automation/light_controller.py 6 0 90
+/usr/local/bin/sunwait sun down 43.921510N 70.084008W; /home/jessebishop/git/home_automation/light_controller.py 3 0 90; /home/jessebishop/git/home_automation/light_controller.py 6 0 90; /home/jessebishop/git/home_automation/light_controller.py 8 0 90
