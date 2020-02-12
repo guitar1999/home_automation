a4217952f075ab728086baf811ef4ab7b63d7ccc
guitar1999 <jessebishop@yahoo.com>
2020-02-11 21:06:31 -0500

    separating barn

--- night_off.sh
+++ night_off.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 /home/jessebishop/git/home_automation/light_controller.py 3 0 0
 /home/jessebishop/git/home_automation/light_controller.py 6 0 0
-/home/jessebishop/git/home_automation/light_controller.py 8 0 0
---
+++ night_off_barn.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/jessebishop/git/home_automation/light_controller.py 8 0 0
